bcb425ce72d3ff527cc83c14376f7c26884ffca8
Oleh Petriv <mr.oleh.petriv@gmail.com>
2021-12-14 10:24:28 +0200

    Add FTX API constants

---
+++ src/main/java/io/microanalysis/constant/FtxApiConstants.java
@@ -0,0 +1,29 @@
+package io.microanalysis.constant;
+
+import org.apache.commons.lang3.builder.ToStringStyle;
+
+/**
+ * Constants used throughout FTX's API.
+ */
+public final class FtxApiConstants {
+
+    /**
+     * Base domain.
+     */
+    public static final String BASE_DOMAIN = "ftx.com";
+
+    /**
+     * REST API base URL.
+     */
+    public static final String API_BASE_URL = "https://" + BASE_DOMAIN;
+
+    /**
+     * Streaming API base URL.
+     */
+    public static final String STREAM_API_BASE_URL = String.format("wss://%s/ws", BASE_DOMAIN);
+
+    /**
+     * Default ToStringStyle used by toString methods.
+     */
+    public static final ToStringStyle TO_STRING_BUILDER_STYLE = ToStringStyle.SHORT_PREFIX_STYLE;
+}
